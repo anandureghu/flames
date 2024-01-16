2e4b69838fc8cf2a42ce84246e96e38128b39e7e
anandureghu <anandukr145@gmail.com>
2024-01-17 00:27:58 +0530

    completed basic logic

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const flameBtn = document.getElementById("flame-btn");
 const firstPerson = document.getElementById("firstPerson");
 const secondPerson = document.getElementById("secondPerson");
 
+const flamesContainer = document.querySelector(".app__flames")
+const resultContainer = document.querySelector(".result")
+
+const sleepTime = 300;
+
 const firstPersonDisplayArea = document.querySelector(
   ".app__display-firstPerson"
 );
@@ -18,8 +23,19 @@ flameBtn.addEventListener("click", async (e) => {
   getValues();
   display();
   await match(); // wait for match to complete
-  
+
+  const flameNumber = getFlamesNumber()
+  await flameIt(flameNumber)
 });
+
+firstPerson.addEventListener("change",()=>{
+  clear()
+})
+
+
+secondPerson.addEventListener("change",()=>{
+  clear()
+})
 
 const getValues = () => {
   firstPersonValues = firstPerson.value.split("").map((letter, i) => {
@@ -51,7 +67,6 @@ const display = () => {
 };
 
 const match = async () => {
-  const sleepTime = 500;
   for (let idx = 0; idx < firstPersonValues.length; idx++) {
     await sleep(sleepTime);
     let fValue = firstPersonValues[idx];
@@ -87,4 +102,65 @@ const match = async () => {
   }
 };
 
+const getFlamesNumber = () => {
+  const firstSum = firstPersonValues.reduce((total, value) => total + (!value.found ? 1 : 0), 0);  
+  const secondSum = secondPersonValues.reduce((total, value) => total + (!value.found ? 1 : 0), 0);  
+  return firstSum + secondSum
+}
+
+const flameIt = async (flameNumber) => {
+  const flames = [{
+    letter: 'F', 
+    value: 'Friendship', 
+    cut: false}, {letter: 'L', value: 'Love', cut: false}, {letter: 'A', value: 'Affection', cut: false}, {letter: 'M', value: 'Marriage', cut: false}, {letter: 'E', value: 'Enemy', cut: false}, {letter: 'S', value: 'Sibling', cut: false}];
+    flamesContainer.innerHTML = "";
+
+    flames.forEach(flame => {
+      flamesContainer.innerHTML += `<div class="flame" id="flame-${flame.letter}">${flame.letter}</div>`;
+      flame.id=`flame-${flame.letter}`;
+    })
+
+    let totalCut = 0;
+    let count = 0;
+
+    while(totalCut < flames.length-1){
+      for(let idx=0; idx<flames.length; idx++){
+
+        const flame = flames[idx];
+        const flameElement = document.getElementById(flame.id)
+        
+        if(!flame.cut){
+          count += 1;
+          console.log(flame.letter, count)
+          flameElement.classList.add("current");
+          await sleep(sleepTime);
+          if(count === flameNumber){
+            console.log("cut ", flame.letter, count)
+            totalCut += 1;
+            count = 0;
+            flame.cut= true;
+            flameElement.classList.add("cut");
+            flameElement.classList.remove("current");
+          }
+          flameElement.classList.remove("current");
+        }
+      }
+    }
+
+    const resultFlame = flames.filter(flame=>!flame.cut)
+    showResult(resultFlame[0].value)
+}
+
+const showResult = (result) => {
+  resultContainer.innerHTML = result;
+}
+
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+
+const clear = () => {
+  flamesContainer.innerHTML= "";
+  resultContainer.innerHTML = "";
+  firstPersonDisplayArea.innerHTML = "";
+  secondPersonDisplayArea.innerHTML = "";
+}
